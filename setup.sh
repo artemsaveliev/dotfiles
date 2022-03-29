605b4a9f49c6f0c716e07d0d6845f5fb650ed979
Artem Saveliev <artemsaveliev@github.com>
2022-03-29 14:22:11 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 alias lintrb="git ls-files -m | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop"
+
+echo "machine goproxy.githubapp.com login nobody password $GH_GH_PAT" >> $HOME/.netrc
