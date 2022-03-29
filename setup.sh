0999f0ddf7e0350a05a1aa1a706605cd892b03be
Artem Saveliev <artemsaveliev@github.com>
2022-03-29 15:05:35 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,9 @@
 
 alias lintrb="git ls-files -m | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop"
 
+export GOPROXY=https://goproxy.githubapp.com/mod,https://proxy.golang.org/,direct
+export GOPRIVATE=
+export GONOPROXY=
+export GONOSUMDB='github.com/github/*'
+
 echo "machine goproxy.githubapp.com login nobody password $GH_GH_PAT" >> $HOME/.netrc
